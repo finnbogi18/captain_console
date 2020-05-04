88422b1b01b3f5c74d0fb235ac0113fe8d41849e
Gisli Thor <gislig18@ru.is>
2020-05-04 11:51:44 +0000

    index templates for manufacturers

--- manufacturers/views.py
+++ manufacturers/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Við erum mega cool!')
+    return render(request,'manufacturers/index.html')
