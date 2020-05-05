2fab02a01a3641ba7806f8019a28f9014d06a839
Atli Snær Jóhannsson <atlisjoh@gmail.com>
2020-05-05 11:41:13 +0000

    Diary

--- captain_console/settings.py
+++ captain_console/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
-    'about.apps.AboutUsConfig'
+    'about.apps.AboutUsConfig',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
