5ac2526cf9a587655f846c0ba15f06cd204e8daf
Konn1nn <54936310+Konn1nn@users.noreply.github.com>
2020-05-07 15:03:15 +0000

    Created a searchfield based of the video tutorial for candy queen. It works but is only located in the store tab.

--- products/views.py
+++ products/views.py
@@ -1,8 +1,18 @@
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from products.models import Product
 
 
 def index(request):
+    if 'search_filter' in request.GET:
+        search_filter = request.GET['search_filter']
+        products = [{
+            'id': x.id,
+            'name': x.name,
+            'price': x.price,
+            'firstImage': x.productimage_set.first().image
+        } for x in Product.objects.filter(name__icontains=search_filter)]
+        return JsonResponse({'data': products})
     context = {'products': Product.objects.all().order_by('name')}
     return render(request, 'products/index.html', context)
 
