f92a9b9b58f177201826337cd428e1e195cb3007
Gisli Thor <gislig18@ru.is>
2020-05-07 09:47:18 +0000

    Diary update

--- accounts/models.py
+++ accounts/models.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
 class SearchHistory(models.Model):
     search = models.CharField(max_length=999)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.search
