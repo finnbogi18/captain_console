e36609dd2b91d58d094045bebfcb71d8a0f5e4b0
Konn1nn <54936310+Konn1nn@users.noreply.github.com>
2020-05-08 14:57:37 +0000

    Ding dong categories og manufaturers filter virkar

--- products/views.py
+++ products/views.py
@@ -1,22 +1,34 @@
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
-from products.models import Product, ProductCategory
+from products.models import Product, ProductCategory, Manufacturer
 from accounts.models import add_history
 
 
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
 def index(request):
     qs = Product.objects.all()
-    category = ProductCategory.objects.all()
+    categories = ProductCategory.objects.all()
+    manufacturers = Manufacturer.objects.all()
     product_search = request.GET.get('product-search')
-    if product_search != '' and product_search is not None:
+    category = request.GET.get('category')
+    manufacturer = request.GET.get('manufacturer')
+    if is_valid_queryparam(product_search):
         current_user = request.user
         add_history(product_search,current_user)
         qs = qs.filter(name__icontains=product_search)
 
+    if is_valid_queryparam(category) and category != 'Choose...':
+        qs = qs.filter(category__name=category)
+
+    if is_valid_queryparam(manufacturer) and manufacturer != 'Choose...':
+        qs = qs.filter(manufacturer__name=manufacturer)
 
     context = {
         'queryset': qs,
-        'category': category,
+        'categories': categories,
+        'manufacturers': manufacturers
     }
     return render(request, 'products/index.html', context)
 
