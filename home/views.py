f78dc54b2fa9878b4691f97d46f53dce84b97364
Gisli Thor <gislig18@ru.is>
2020-05-08 14:16:01 +0000

    Nav search

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
+    product_search = request.GET.get('product-search')
+    if product_search != '' and product_search is not None:
+        return redirect('product-index')
     return render(request, 'home/index.html')
