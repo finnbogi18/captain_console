16cee40afd5a1bc8cf1e6f756a135d7a110c86a6
Konn1nn <54936310+Konn1nn@users.noreply.github.com>
2020-05-08 09:39:12 +0000

    New search engine

--- products/views.py
+++ products/views.py
@@ -4,16 +4,14 @@ from products.models import Product
 
 
 def index(request):
-    if 'search_filter' in request.GET:
-        search_filter = request.GET['search_filter']
-        products = [{
-            'id': x.id,
-            'name': x.name,
-            'price': '$ ' + str(x.price),
-            'firstImage': x.productimage_set.first().image
-        } for x in Product.objects.filter(name__icontains=search_filter)]
-        return JsonResponse({'data': products})
-    context = {'products': Product.objects.all().order_by('name')}
+    qs = Product.objects.all()
+    product_search = request.GET.get('product-search')
+    print(product_search)
+    if product_search != '' and product_search is not None:
+        qs = qs.filter(name__icontains=product_search)
+    context = {
+        'queryset': qs
+    }
     return render(request, 'products/index.html', context)
 
 
