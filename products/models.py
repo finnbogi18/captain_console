e9f0fc9e0068b711a121058e2f7cb59ad2f81884
Konn1nn <54936310+Konn1nn@users.noreply.github.com>
2020-05-13 14:53:14 +0000

    Fixed a bug in product image

--- products/models.py
+++ products/models.py
@@ -58,6 +58,6 @@ class ProductImage(models.Model):
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.product_id.name
+        return self.image
 
 
