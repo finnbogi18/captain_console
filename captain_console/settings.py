d159210b4a00a538888f905c8254b0e6b1c2d82b
Atli Snær Jóhannsson <atlisjoh@gmail.com>
2020-05-15 15:12:54 +0000

    settings debug false

--- captain_console/settings.py
+++ captain_console/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5v(c761ejnv80_-8kwfvjo=u**_3f1tj(ss-)!z13v2kcsy=kj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
