693d4a15ef963b30dced1779d871a67018221f98
Atli Snær Jóhannsson <atlisjoh@gmail.com>
2020-05-15 13:45:11 +0000

    laga klúður frá Finnboga

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
-from cart.views import add_to_cart, remove_one_item_from_cart, remove_all_item_from_cart
+from cart.views import add_to_cart, remove_one_item_from_cart
 
 urlpatterns = [
     path('', views.index, name="product-index"),
     path('<int:id>', views.get_product_by_id, name="product_details"),
     path('<slug>', add_to_cart, name="add-to-cart"),
     path('<slug>/remove-one', remove_one_item_from_cart, name="remove-one-cart"),
-    path('<slug>/remove-all', remove_all_item_from_cart, name="remove-all-item-cart"),
 ]
